1588ffae2774cc1cc40f5285de6780de1a75dfb7
Ricardo Duque <theapsu@gmail.com>
2023-04-17 09:23:52 -0400

    first commit

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ const { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } =  require("./config.j
 
 const dbConfig = {
   host: DB_HOST,
-  user: DB_USER,
+  user: DB_USER, 
   password: DB_PASSWORD,
   port: DB_PORT, 
   database: DB_NAME
