4539b53fab6b952fc5f2c66592481db3d5a5ab9f
Ricardo Duque <theapsu@gmail.com>
2023-04-22 19:40:33 -0400

    post verify

--- src/routes/paypal.js
+++ src/routes/paypal.js
@@ -2,6 +2,6 @@ const express = require('express');
 const PaypalController = require('../controllers/PaypalController.js');
 const router = express.Router();
 
-router.get('/', PaypalController.verify);
+router.post('/', PaypalController.verify);
 
 module.exports = router;
