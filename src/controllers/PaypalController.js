25c963895e0089931f9bd399d145742587b2860f
Ricardo Duque <theapsu@gmail.com>
2023-04-23 16:45:19 -0400

    verificar estado v.2

--- src/controllers/PaypalController.js
+++ src/controllers/PaypalController.js
@@ -103,12 +103,12 @@ function hasActiveSubscription(req, res){
         const actualDate = new Date();
         const cutDate = new Date(rows[0].cut_date);
         if(actualDate > cutDate){
-          res.status(403).send({ status: false, msg: 'Debes tener una suscripcion activa para continuar' });
+          res.status(403).send({ status: false, msg: 'Debes tener una suscripción activa para continuar.' });
         }else{
-          res.status(200).send({ status: false });
+          res.status(200).send({ status: true, msg: 'Ya posee una suscripción activa.' });
         }
       }else{
-        res.status(403).send({ status: false, msg: 'Debes tener una suscripcion activa para continuar' });
+        res.status(403).send({ status: false, msg: 'Debes tener una suscripción activa para continuar.' });
       }
     })
   })
